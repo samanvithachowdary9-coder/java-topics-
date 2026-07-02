5afd4e7bee9826c5d75ce1235d39fd78136ba2f3
samanvithachowdary9-coder <samanvithachowdary9@gmail.com>
2026-07-02 10:18:06 +0530

    Add examples for array operations in Array.java

---
+++ Array.java
@@ -0,0 +1,27 @@
+// Indexed Character Printing in an Array
+class Main {
+    public static void main(String[] args) {
+        int [] x = {1,2,3,4,5};
+        System.out.println(x[4]);
+    }
+}
+//Ordered Character printing in an Array
+class Main {
+    public static void main(String[] args) {
+        int [] x = {1,2,3,4,5};
+        for(int i = 0; i< x.length;i++) {
+        System.out.println(x[i]);
+        }
+    }
+}
+//Sum of Array
+class Main {
+    public static void main(String[] args) {
+        int [] x = {1,2,3,4,5};
+        int sum = 0;
+        for(int i = 0; i< x.length;i++) {
+            sum += x[i];
+        System.out.println(sum);
+        }
+    }
+}
